e48ebe587c7a45f34574ec47ec773f135fdaf178
huangpeng <huangpeng@meizu.com>
2020-04-21 23:08:37 +0800

    init ts project #0

---
+++ src/page/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="header">
+      {{ title }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  components: {
+  },
+  data() {
+    return {
+      title: '首页'
+    }
+  }
+};
+</script>
+<style lang="scss">
+.container{
+  .header{
+    color: red;
+  }
+}
+</style>
